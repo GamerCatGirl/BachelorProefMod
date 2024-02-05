3da3a89a4bb4a001ec8a356c562b9422c1eb7458
Silken <silken_heynderickx@icloud.com>
2024-02-05 15:00:24 +0100

    robot tameable

--- src/main/java/siheynde/bachelorproefmod/entity/robot/RobotEntity.java
+++ src/main/java/siheynde/bachelorproefmod/entity/robot/RobotEntity.java
@@ -1,16 +1,24 @@
 package siheynde.bachelorproefmod.entity.robot;
 
+import net.minecraft.entity.EntityStatuses;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.goal.FollowOwnerGoal;
 import net.minecraft.entity.ai.goal.LookAtEntityGoal;
 import net.minecraft.entity.ai.goal.SwimGoal;
+import net.minecraft.entity.ai.goal.WanderAroundFarGoal;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.entity.passive.TameableEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.world.EntityView;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
@@ -31,8 +39,12 @@ public class RobotEntity extends TameableEntity {
     @Override
     protected void initGoals() {
         this.goalSelector.add(0, new SwimGoal(this)); //lower the number to make it a higher priority
-        this.goalSelector.add(1, new LookAtEntityGoal(this, PlayerEntity.class, 4f));
+        this.goalSelector.add(2, new WanderAroundFarGoal(this, 1.0));
+        this.goalSelector.add(4, new LookAtEntityGoal(this, PlayerEntity.class, 4f));
+        this.goalSelector.add(3, new FollowOwnerGoal(this, 1.0, 10.0f, 2.0f, false));
     }
+
+    //TODO: make happy Goal when new function received
 
     @Nullable
     @Override
@@ -40,14 +52,58 @@ public class RobotEntity extends TameableEntity {
         return ModEntities.ROBOT.create(world);
     }
 
+    //TODO: mob thinks his arm is the front of the mob
+
+    public ActionResult interactMob(PlayerEntity player, Hand hand) {
+        ItemStack itemStack = player.getStackInHand(hand);
+        Item item = itemStack.getItem();
+
+
+        if (this.getWorld().isClient) {
+            boolean bl = this.isOwner(player) || this.isTamed() || (itemStack.isOf(Items.IRON_INGOT) && !this.isTamed());
+            return bl ? ActionResult.CONSUME : ActionResult.PASS;
+        }
+        if (this.isOwner(player)) {
+            // TODO: open GUI
+            System.out.println("Owner");
+        }
+        else if (this.isTamed()) {
+            // TODO: View progress other player
+            System.out.println("Tamed");
+        }
+        else if (item == Items.IRON_INGOT && !this.isTamed()) {
+            //super.interactMob(player, hand);
+
+
+            if (!player.getAbilities().creativeMode) {
+                itemStack.decrement(1);
+            }
+            this.setOwner(player);
+            this.getWorld().sendEntityStatus(this, EntityStatuses.ADD_POSITIVE_PLAYER_REACTION_PARTICLES);
+
+
+            player.addExperienceLevels(1);
+        }
+
+        return ActionResult.SUCCESS;
+
+
+    }
+
     @Override
     protected void initDataTracker() {
         super.initDataTracker();
     }
 
-
     @Override
     public EntityView method_48926() {
-        return null;
+        return this.getWorld();
     }
+
+    @Nullable
+    @Override
+    public LivingEntity getOwner() {
+        return super.getOwner();
+    }
+
 }

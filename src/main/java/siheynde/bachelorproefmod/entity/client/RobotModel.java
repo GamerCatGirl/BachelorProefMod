d00d32aa3be7ab97c9ac7a2d1f6f0469db4b7969
Silken <silken_heynderickx@icloud.com>
2024-02-06 15:19:19 +0100

    pivot robot so his face looks at you

--- src/main/java/siheynde/bachelorproefmod/entity/client/RobotModel.java
+++ src/main/java/siheynde/bachelorproefmod/entity/client/RobotModel.java
@@ -17,23 +17,23 @@ public class RobotModel<T extends RobotEntity> extends SinglePartEntityModel<T> 
 	public static TexturedModelData getTexturedModelData() {
 		ModelData modelData = new ModelData();
 		ModelPartData modelPartData = modelData.getRoot();
-		ModelPartData Robot = modelPartData.addChild("Robot", ModelPartBuilder.create(), ModelTransform.pivot(8.0F, 24.0F, -8.0F));
+		ModelPartData Robot = modelPartData.addChild("Robot", ModelPartBuilder.create(), ModelTransform.of(1.0F, 24.0F, -1.0F, 0.0F, -1.5272F, 0.0F));
 
 		ModelPartData Legs = Robot.addChild("Legs", ModelPartBuilder.create().uv(2, 4).cuboid(-1.0F, 4.0F, -4.0F, 2.0F, 4.0F, 2.0F, new Dilation(0.0F))
-		.uv(0, 2).cuboid(-1.0F, 4.0F, 1.0F, 2.0F, 4.0F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(-8.0F, -8.0F, 8.0F));
+				.uv(0, 2).cuboid(-1.0F, 4.0F, 1.0F, 2.0F, 4.0F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -8.0F, 1.0F));
 
-		ModelPartData Body = Robot.addChild("Body", ModelPartBuilder.create().uv(0, 0).cuboid(-4.0F, -5.0F, -5.0F, 9.0F, 9.0F, 9.0F, new Dilation(0.0F)), ModelTransform.pivot(-8.0F, -8.0F, 8.0F));
+		ModelPartData Body = Robot.addChild("Body", ModelPartBuilder.create().uv(0, 0).cuboid(-4.0F, -5.0F, -5.0F, 9.0F, 9.0F, 9.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -8.0F, 1.0F));
 
 		ModelPartData Arms = Robot.addChild("Arms", ModelPartBuilder.create().uv(0, 2).cuboid(-1.0F, -0.5F, -6.5F, 2.0F, 9.0F, 2.0F, new Dilation(0.0F))
-		.uv(0, 2).cuboid(-1.0F, -0.5F, 4.5F, 2.0F, 9.0F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(-8.0F, -11.5F, 7.5F));
+				.uv(0, 2).cuboid(-1.0F, -0.5F, 4.5F, 2.0F, 9.0F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -11.5F, 0.5F));
 
 		ModelPartData Antene = Robot.addChild("Antene", ModelPartBuilder.create().uv(0, 1).cuboid(0.0F, -7.0F, -1.0F, 1.0F, 2.0F, 1.0F, new Dilation(0.0F))
-		.uv(0, 3).cuboid(0.0F, -9.0F, -2.0F, 1.0F, 2.0F, 3.0F, new Dilation(0.0F)), ModelTransform.pivot(-8.0F, -8.0F, 8.0F));
+				.uv(0, 3).cuboid(0.0F, -9.0F, -2.0F, 1.0F, 2.0F, 3.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -8.0F, 1.0F));
 
 		ModelPartData Energy = Robot.addChild("Energy", ModelPartBuilder.create().uv(0, 2).cuboid(0.0F, -11.0F, 2.0F, 1.0F, 1.0F, 2.0F, new Dilation(0.0F))
-		.uv(0, 2).cuboid(0.0F, -12.0F, 3.0F, 1.0F, 1.0F, 2.0F, new Dilation(0.0F))
-		.uv(0, 2).cuboid(0.0F, -13.0F, 4.0F, 1.0F, 1.0F, 2.0F, new Dilation(0.0F))
-		.uv(0, 2).cuboid(0.0F, -14.0F, 5.0F, 1.0F, 1.0F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(-8.0F, -8.0F, 8.0F));
+				.uv(0, 2).cuboid(0.0F, -12.0F, 3.0F, 1.0F, 1.0F, 2.0F, new Dilation(0.0F))
+				.uv(0, 2).cuboid(0.0F, -13.0F, 4.0F, 1.0F, 1.0F, 2.0F, new Dilation(0.0F))
+				.uv(0, 2).cuboid(0.0F, -14.0F, 5.0F, 1.0F, 1.0F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -8.0F, 1.0F));
 		return TexturedModelData.of(modelData, 64, 64);
 	}
 	@Override

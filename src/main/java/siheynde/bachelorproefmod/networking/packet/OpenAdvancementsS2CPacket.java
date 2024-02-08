db72c5d1fba6bae45a44cdec142d6aff8ad362a8
Silken <silken_heynderickx@icloud.com>
2024-02-08 19:54:09 +0100

    Select robot tab advancements

--- src/main/java/siheynde/bachelorproefmod/networking/packet/OpenAdvancementsS2CPacket.java
+++ src/main/java/siheynde/bachelorproefmod/networking/packet/OpenAdvancementsS2CPacket.java
@@ -1,12 +1,15 @@
 package siheynde.bachelorproefmod.networking.packet;
 
+import net.minecraft.advancement.AdvancementEntry;
+import net.minecraft.advancement.AdvancementManager;
+import net.minecraft.advancement.PlacedAdvancement;
 import net.minecraft.client.MinecraftClient;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
-import net.minecraft.client.gui.screen.advancement.AdvancementTab;
 import net.minecraft.client.gui.screen.advancement.AdvancementsScreen;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.PacketByteBuf;
-import net.minecraft.screen.ScreenHandler;
+
+import java.util.Collection;
 
 public class OpenAdvancementsS2CPacket {
 
@@ -15,8 +18,17 @@ public class OpenAdvancementsS2CPacket {
         client.execute(() -> {
             // Everything in this lambda is run on the render thread
             assert client.player != null;
-            AdvancementsScreen screen = new AdvancementsScreen(client.player.networkHandler.getAdvancementHandler());
+            AdvancementsScreen screen = new AdvancementsScreen(handler.getAdvancementHandler());
             client.setScreen(screen);
+            //AdvancementManager manager //= client.player.networkHandler
+            Collection<PlacedAdvancement> advancements = handler.getAdvancementHandler().getManager().getAdvancements();
+            advancements.forEach(advancement -> {
+                AdvancementEntry entry = advancement.getAdvancementEntry();
+                String id = entry.toString();
+                if (id.equals("bachelorproef:course1/root")) {
+                    screen.selectTab(entry);
+                }
+            });
 
         });
     }

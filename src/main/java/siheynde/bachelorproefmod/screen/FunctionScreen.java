23635f3e521e49974fdbbc73ef342987839df86d
Silken <silken_heynderickx@icloud.com>
2024-02-11 17:44:37 +0100

    add logic inputPredict

--- src/main/java/siheynde/bachelorproefmod/screen/FunctionScreen.java
+++ src/main/java/siheynde/bachelorproefmod/screen/FunctionScreen.java
@@ -1,16 +1,22 @@
 package siheynde.bachelorproefmod.screen;
 
 import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.datafixers.types.Func;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.gui.screen.recipebook.RecipeBookProvider;
 import net.minecraft.client.gui.screen.recipebook.RecipeBookWidget;
+import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.client.gui.widget.TexturedButtonWidget;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.screen.AbstractRecipeScreenHandler;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.packet.c2s.play.RenameItemC2SPacket;
+import net.minecraft.screen.*;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.screen.slot.SlotActionType;
 import net.minecraft.text.OrderedText;
@@ -18,18 +24,24 @@ import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.text.TextContent;
 import net.minecraft.util.Identifier;
+import org.lwjgl.glfw.GLFW;
 import siheynde.bachelorproefmod.BachelorProef;
 import siheynde.bachelorproefmod.screen.FunctionScreenHandler;
 
 import java.util.List;
 
+@Environment(value=EnvType.CLIENT)
 public class FunctionScreen
         extends HandledScreen<FunctionScreenHandler>
-        implements RecipeBookProvider {
+        implements RecipeBookProvider, ScreenHandlerListener {
 
     protected static final int backgroundWidth = 248;
     private static final Identifier TEXTURE = new Identifier(BachelorProef.MOD_ID, "textures/gui/function_screen.png");
+    private static final Identifier TEXT_FIELD_TEXTURE = new Identifier("container/anvil/text_field");
+    private TextFieldWidget predictInputField;
+    private TextFieldWidget InvestigateInputField;
     private final RecipeBookWidget recipeBook = new RecipeBookWidget();
+    private TextFieldWidget predictField;
     private boolean narrow;
 
     public FunctionScreen(FunctionScreenHandler handler, PlayerInventory inventory, Text title) {
@@ -39,19 +51,70 @@ public class FunctionScreen
     @Override
     protected void init() {
         super.init();
+        handler.addListener(this);
+
+
+        this.predictField = new TextFieldWidget(this.textRenderer, x + 33, y + 13, 103, 12, Text.translatable("container.repair"));
+        this.predictField.setFocusUnlocked(false);
+        this.predictField.setEditableColor(-1);
+        this.predictField.setUneditableColor(-1);
+        this.predictField.setDrawsBackground(false);
+        this.predictField.setMaxLength(50);
+        this.predictField.setChangedListener(this::onRenamed);
+        this.predictField.setText("testInput");
+        this.addSelectableChild(this.predictField);
+        this.setInitialFocus(this.predictField);
+        this.predictField.setEditable(((FunctionScreenHandler)this.handler).getSlot(0).hasStack());
+    }
+
+    @Override
+    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        super.render(context, mouseX, mouseY, delta);
+        renderBackground(context, mouseX, mouseY, delta);
+        this.renderForeground(context, mouseX, mouseY, delta);
+        this.drawMouseoverTooltip(context, mouseX, mouseY);
+    }
+
+    public void renderForeground(DrawContext context, int mouseX, int mouseY, float delta) {
+        this.predictField.render(context, mouseX, mouseY, delta);
+    }
+
+    @Override
+    public void resize(MinecraftClient client, int width, int height) {
+        String string = this.predictField.getText();
+        this.init(client, width, height);
+        this.predictField.setText(string);
+    }
+
+    @Override
+    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
+        if (keyCode == GLFW.GLFW_KEY_ESCAPE) {
+            //this.client.player.closeHandledScreen();
+        }
+        if (this.predictField.keyPressed(keyCode, scanCode, modifiers) || this.predictField.isActive()) {
+            return true;
+        }
+        return super.keyPressed(keyCode, scanCode, modifiers);
+    }
+
+    private void onRenamed(String name) {
+        Slot slot = ((FunctionScreenHandler)this.handler).getSlot(0);
+        if (!slot.hasStack()) {
+            return;
+        }
+        String string = name;
+        if (!slot.getStack().hasCustomName() && string.equals(slot.getStack().getName().getString())) {
+            string = "";
+        }
+        if (((FunctionScreenHandler)this.handler).setNewPredict(string)) {
+            //this.client.player.networkHandler.sendPacket(new RenameItemC2SPacket(string));
+        }
     }
 
     @Override
     public void handledScreenTick() {
         super.handledScreenTick();
         //this.recipeBook.update();
-    }
-
-    @Override
-    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-        renderBackground(context, mouseX, mouseY, delta);
-        super.render(context, mouseX, mouseY, delta);
-        drawMouseoverTooltip(context, mouseX, mouseY);
     }
 
     @Override
@@ -63,8 +126,26 @@ public class FunctionScreen
         int y = (height - backgroundHeight) / 2;
 
         context.drawTexture(TEXTURE, x, y, 0, 0, backgroundWidth, backgroundHeight);
+        context.drawGuiTexture(TEXT_FIELD_TEXTURE, this.x + 31, this.y + 9, 110, 16);
+
 
         renderProgressArrow(context, x, y);
+    }
+
+    private void onPredictInput(String name) {
+        Slot slot = handler.getSlot(0);
+        if (!slot.hasStack()) {
+            return;
+        }
+        String string = name;
+        if (!slot.getStack().hasCustomName() && string.equals(slot.getStack().getName().getString())) {
+            string = "";
+        }
+        if (handler.setNewPredict(string)) {
+            //this.client.player.networkHandler.sendPacket(new RenameItemC2SPacket(string));
+
+            //Rename item
+        }
     }
 
     @Override
@@ -115,5 +196,15 @@ public class FunctionScreen
     @Override
     public RecipeBookWidget getRecipeBookWidget() {
         return this.recipeBook;
+    }
+
+    @Override
+    public void onSlotUpdate(ScreenHandler handler, int slotId, ItemStack stack) {
+
+    }
+
+    @Override
+    public void onPropertyUpdate(ScreenHandler handler, int property, int value) {
+
     }
 }
--- src/main/java/siheynde/bachelorproefmod/screen/FunctionScreenHandler.java
+++ src/main/java/siheynde/bachelorproefmod/screen/FunctionScreenHandler.java
@@ -1,13 +1,18 @@
 package siheynde.bachelorproefmod.screen;
 
+import net.minecraft.SharedConstants;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.CraftingInventory;
 import net.minecraft.inventory.CraftingResultInventory;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.inventory.RecipeInputInventory;
+import net.minecraft.item.EnchantedBookItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.packet.s2c.play.ScreenHandlerSlotUpdateS2CPacket;
 import net.minecraft.recipe.*;
@@ -16,21 +21,18 @@ import net.minecraft.screen.*;
 import net.minecraft.screen.slot.CraftingResultSlot;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.Util;
 import net.minecraft.world.World;
 import siheynde.bachelorproefmod.block.ModBlocks;
 
+import java.util.Map;
 import java.util.Optional;
 
-public class FunctionScreenHandler extends AbstractRecipeScreenHandler<RecipeInputInventory> {
-    public static final int RESULT_ID = 0;
-    private static final int INPUT_START = 1;
-    private static final int INPUT_END = 10;
-    private static final int INVENTORY_START = 10;
-    private static final int INVENTORY_END = 37;
-    private static final int HOTBAR_START = 37;
-    private static final int HOTBAR_END = 46;
-    private final RecipeInputInventory input = new CraftingInventory(this, 3, 3);
-    private final CraftingResultInventory result = new CraftingResultInventory();
+public class FunctionScreenHandler extends ScreenHandler {
+    private String newPredict;
+    //private final RecipeInputInventory input = new CraftingInventory(this, 3, 3);
+    //private final CraftingResultInventory result = new CraftingResultInventory();
 
     public FunctionScreenHandler(int syncId, PlayerInventory inventory, PacketByteBuf buf) {
         this(syncId, inventory, inventory.player.getWorld().getBlockEntity(buf.readBlockPos()));
@@ -48,81 +50,60 @@ public class FunctionScreenHandler extends AbstractRecipeScreenHandler<RecipeInp
         super(ModScreenHandlers.FUNCTION_SCREEN, syncId);
     }
 
+    public void updateResult() {
+        //ItemStack itemStack = this.input.getStack(0);
+        //this.levelCost.set(1);
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        //if (itemStack.isEmpty()) {
+        //    this.output.setStack(0, ItemStack.EMPTY);
+        //    this.levelCost.set(0);
+        //    return;
+        //}
+        //ItemStack itemStack2 = itemStack.copy();
+        //ItemStack itemStack3 = this.input.getStack(1);
+        //Map<Enchantment, Integer> map = EnchantmentHelper.get(itemStack2);
+        //j += itemStack.getRepairCost() + (itemStack3.isEmpty() ? 0 : itemStack3.getRepairCost());
+        //this.repairItemUsage = 0;
+        if (this.newPredict == null || Util.isBlank(this.newPredict)) {
+            // predictInputField is empty
+        } else if (!this.newPredict.equals("0")) {
 
-
-    protected static void updateResult(ScreenHandler handler, World world, PlayerEntity player, RecipeInputInventory craftingInventory, CraftingResultInventory resultInventory) {
-        if (world.isClient) {
-            return;
         }
-        ServerPlayerEntity serverPlayerEntity = (ServerPlayerEntity)player;
-        ItemStack itemStack = ItemStack.EMPTY;
-        Optional<RecipeEntry<CraftingRecipe>> optional = world.getServer().getRecipeManager().getFirstMatch(RecipeType.CRAFTING, craftingInventory, world);
-        if (optional.isPresent()) {
-            ItemStack itemStack2;
-            RecipeEntry<CraftingRecipe> recipeEntry = optional.get();
-            CraftingRecipe craftingRecipe = recipeEntry.value();
-            if (resultInventory.shouldCraftRecipe(world, serverPlayerEntity, recipeEntry) && (itemStack2 = craftingRecipe.craft(craftingInventory, world.getRegistryManager())).isItemEnabled(world.getEnabledFeatures())) {
-                itemStack = itemStack2;
+        //this.levelCost.set(j + i);
+        //this.sendContentUpdates();
+    }
+
+    public boolean setNewPredict(String newItemName) {
+        String string = FunctionScreenHandler.sanitize(newItemName);
+        if (string == null || string.equals(this.newPredict)) {
+            return false;
+        }
+        this.newPredict = string;
+        if (this.getSlot(2).hasStack()) {
+            ItemStack itemStack = this.getSlot(2).getStack();
+            if (Util.isBlank(string)) {
+                itemStack.removeCustomName();
+            } else {
+                itemStack.setCustomName(Text.literal(string));
             }
         }
-        resultInventory.setStack(0, itemStack);
-        handler.setPreviousTrackedSlot(0, itemStack);
-        serverPlayerEntity.networkHandler.sendPacket(new ScreenHandlerSlotUpdateS2CPacket(handler.syncId, handler.nextRevision(), 0, itemStack));
+        this.updateResult();
+        return true;
     }
 
-    @Override
-    public void populateRecipeFinder(RecipeMatcher finder) {
-        this.input.provideRecipeInputs(finder);
+    private static String sanitize(String name) {
+        String string = SharedConstants.stripInvalidChars(name);
+        if (string.length() <= 50) {
+            return string;
+        }
+        return null;
     }
-
-    @Override
-    public void clearCraftingSlots() {
-        this.input.clear();
-        this.result.clear();
-    }
-
-    @Override
-    public boolean matches(RecipeEntry<? extends Recipe<RecipeInputInventory>> recipe) {
-        //return recipe.value().matches(this.input, this.player.getWorld());
-        return false;
-    }
-
 
     @Override
     public ItemStack quickMove(PlayerEntity player, int slot) {
-        ItemStack itemStack = ItemStack.EMPTY;
-        Slot slot2 = (Slot)this.slots.get(slot);
-        if (slot2 != null && slot2.hasStack()) {
-            ItemStack itemStack2 = slot2.getStack();
-            itemStack = itemStack2.copy();
-            if (slot == 0) {
-                //this.context.run((world, pos) -> itemStack2.getItem().onCraftByPlayer(itemStack2, (World)world, player));
-                if (!this.insertItem(itemStack2, 10, 46, true)) {
-                    return ItemStack.EMPTY;
-                }
-                slot2.onQuickTransfer(itemStack2, itemStack);
-            } else if (slot >= 10 && slot < 46 ? !this.insertItem(itemStack2, 1, 10, false) && (slot < 37 ? !this.insertItem(itemStack2, 37, 46, false) : !this.insertItem(itemStack2, 10, 37, false)) : !this.insertItem(itemStack2, 10, 46, false)) {
-                return ItemStack.EMPTY;
-            }
-            if (itemStack2.isEmpty()) {
-                slot2.setStack(ItemStack.EMPTY);
-            } else {
-                slot2.markDirty();
-            }
-            if (itemStack2.getCount() == itemStack.getCount()) {
-                return ItemStack.EMPTY;
-            }
-            slot2.onTakeItem(player, itemStack2);
-            if (slot == 0) {
-                player.dropItem(itemStack2, false);
-            }
-        }
-        return itemStack;
-    }
-
-    @Override
-    public boolean canInsertIntoSlot(ItemStack stack, Slot slot) {
-        return slot.inventory != this.result && super.canInsertIntoSlot(stack, slot);
+        return null;
     }
 
     @Override
@@ -130,33 +111,4 @@ public class FunctionScreenHandler extends AbstractRecipeScreenHandler<RecipeInp
         return true;
     }
 
-    @Override
-    public int getCraftingResultSlotIndex() {
-        return 0;
-    }
-
-    @Override
-    public int getCraftingWidth() {
-        return this.input.getWidth();
-    }
-
-    @Override
-    public int getCraftingHeight() {
-        return this.input.getHeight();
-    }
-
-    @Override
-    public int getCraftingSlotCount() {
-        return 10;
-    }
-
-    @Override
-    public RecipeBookCategory getCategory() {
-        return RecipeBookCategory.CRAFTING;
-    }
-
-    @Override
-    public boolean canInsertIntoSlot(int index) {
-        return index != this.getCraftingResultSlotIndex();
-    }
 }

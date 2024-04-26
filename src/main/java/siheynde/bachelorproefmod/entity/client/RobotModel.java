eed86fec9e28dbc85403e717f9964b216a908d6d
Silken <silken_heynderickx@icloud.com>
2024-04-26 09:21:20 +0200

    robot hold block

--- src/main/java/siheynde/bachelorproefmod/entity/client/RobotModel.java
+++ src/main/java/siheynde/bachelorproefmod/entity/client/RobotModel.java
@@ -12,33 +12,77 @@ import siheynde.bachelorproefmod.entity.robot.RobotEntity;
 // Paste this class into your mod and generate all required imports
 public class RobotModel<T extends RobotEntity> extends SinglePartEntityModel<T> {
 	private final ModelPart Robot;
+	//private final ModelPart head;
+	private final ModelPart rightArm;
+	private final ModelPart leftArm;
 	public RobotModel(ModelPart root) {
 		this.Robot = root.getChild("Robot");
+		//this.head = root.getChild(EntityModelPartNames.HEAD);
+		this.rightArm = root.getChild(EntityModelPartNames.RIGHT_ARM);
+		this.leftArm = root.getChild(EntityModelPartNames.LEFT_ARM);
 	}
 	public static TexturedModelData getTexturedModelData() {
 		ModelData modelData = new ModelData();
 		ModelPartData modelPartData = modelData.getRoot();
-		ModelPartData Robot = modelPartData.addChild("Robot", ModelPartBuilder.create(), ModelTransform.of(1.0F, 24.0F, -1.0F, 0.0F, -1.5272F, 0.0F));
+		ModelPartData Robot = modelPartData.addChild(
+				"Robot",
+				ModelPartBuilder.create(),
+				ModelTransform.of(1.0F, 24.0F, -1.0F, 0.0F, -1.5272F, 0.0F));
 
-		ModelPartData Legs = Robot.addChild("Legs", ModelPartBuilder.create().uv(2, 4).cuboid(-1.0F, 4.0F, -4.0F, 2.0F, 4.0F, 2.0F, new Dilation(0.0F))
+		ModelPartData Legs = modelPartData.addChild("Legs", ModelPartBuilder.create().uv(2, 4)
+				.cuboid(-1.0F, 4.0F, -4.0F, 2.0F, 4.0F, 2.0F, new Dilation(0.0F))
 				.uv(0, 2).cuboid(-1.0F, 4.0F, 1.0F, 2.0F, 4.0F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -8.0F, 1.0F));
 
-		ModelPartData Body = Robot.addChild("Body", ModelPartBuilder.create().uv(0, 0).cuboid(-4.0F, -5.0F, -5.0F, 9.0F, 9.0F, 9.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -8.0F, 1.0F));
+		ModelPartData Body = modelPartData.addChild(EntityModelPartNames.BODY, ModelPartBuilder.create().uv(0, 0)
+				.cuboid(-4.0F, -5.0F, -5.0F, 9.0F, 9.0F, 9.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -8.0F, 1.0F));
 
-		ModelPartData ArmsRigth = Robot.addChild(EntityModelPartNames.RIGHT_ARM, ModelPartBuilder.create().uv(0, 2).cuboid(-1.0F, -0.5F, -6.5F, 2.0F, 9.0F, 2.0F, new Dilation(0.0F))
-				.uv(0, 2).cuboid(-1.0F, -0.5F, 4.5F, 2.0F, 9.0F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -11.5F, 0.5F));
+		ModelPartData ArmsRigth = modelPartData
+				.addChild(EntityModelPartNames.RIGHT_ARM, ModelPartBuilder.create().uv(0, 2)
+				.cuboid(-1.0F, -0.5F, -6.5F, 2.0F, 9.0F, 2.0F, new Dilation(0.0F))
+				.uv(0, 2)
+						.cuboid(-1.0F, -0.5F, 4.5F, 2.0F, 9.0F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -11.5F, 0.5F));
+                                                                                                                                                           //duwt de arm naar achter (pivot x)
+		ModelPartData ArmsLeft = modelPartData.addChild(EntityModelPartNames.LEFT_ARM, ModelPartBuilder.create().uv(0, 2)
+				.cuboid(-1.0F, -0.5F, -6.5F, 9.0F, 2.0F, 2.0F, new Dilation(0.0F))
+				.uv(0, 2)
+				.cuboid(-1.0F, -0.5F, 4.5F, 9.0F, 2.0F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -11.5F, 0.5F));
 
-		ModelPartData ArmsLeft = Robot.addChild(EntityModelPartNames.LEFT_ARM, ModelPartBuilder.create().uv(0, 2).cuboid(-1.0F, -0.5F, -6.5F, 2.0F, 9.0F, 2.0F, new Dilation(0.0F))
-				.uv(0, 2).cuboid(-1.0F, -0.5F, 4.5F, 2.0F, 9.0F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -11.5F, 0.5F));
 
-
-		ModelPartData Antene = Robot.addChild("Antene", ModelPartBuilder.create().uv(0, 1).cuboid(0.0F, -7.0F, -1.0F, 1.0F, 2.0F, 1.0F, new Dilation(0.0F))
+		ModelPartData Antene = modelPartData.addChild("Antene", ModelPartBuilder.create().uv(0, 1).cuboid(0.0F, -7.0F, -1.0F, 1.0F, 2.0F, 1.0F, new Dilation(0.0F))
 				.uv(0, 3).cuboid(0.0F, -9.0F, -2.0F, 1.0F, 2.0F, 3.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -8.0F, 1.0F));
 
-		ModelPartData Energy = Robot.addChild("Energy", ModelPartBuilder.create().uv(0, 2).cuboid(0.0F, -11.0F, 2.0F, 1.0F, 1.0F, 2.0F, new Dilation(0.0F))
+		ModelPartData Energy = modelPartData.addChild("Energy", ModelPartBuilder.create().uv(0, 2).cuboid(0.0F, -11.0F, 2.0F, 1.0F, 1.0F, 2.0F, new Dilation(0.0F))
 				.uv(0, 2).cuboid(0.0F, -12.0F, 3.0F, 1.0F, 1.0F, 2.0F, new Dilation(0.0F))
 				.uv(0, 2).cuboid(0.0F, -13.0F, 4.0F, 1.0F, 1.0F, 2.0F, new Dilation(0.0F))
 				.uv(0, 2).cuboid(0.0F, -14.0F, 5.0F, 1.0F, 1.0F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -8.0F, 1.0F));
+		Robot.addChild("Legs", ModelPartBuilder.create().uv(2, 4)
+				.cuboid(-1.0F, 4.0F, -4.0F, 2.0F, 4.0F, 2.0F, new Dilation(0.0F))
+				.uv(0, 2).cuboid(-1.0F, 4.0F, 1.0F, 2.0F, 4.0F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -8.0F, 1.0F));
+
+		Robot.addChild(EntityModelPartNames.BODY, ModelPartBuilder.create().uv(0, 0)
+				.cuboid(-4.0F, -5.0F, -5.0F, 9.0F, 9.0F, 9.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -8.0F, 1.0F));
+
+		Robot
+				.addChild(EntityModelPartNames.RIGHT_ARM, ModelPartBuilder.create().uv(0, 2)
+						.cuboid(-1.0F, -0.5F, -6.5F, 2.0F, 9.0F, 2.0F, new Dilation(0.0F))
+						.uv(0, 2)
+						.cuboid(-1.0F, -0.5F, 4.5F, 2.0F, 9.0F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -11.5F, 0.5F));
+		//duwt de arm naar achter (pivot x)
+		Robot.addChild(EntityModelPartNames.LEFT_ARM, ModelPartBuilder.create().uv(0, 2)
+				.cuboid(-1.0F, -0.5F, -6.5F, 2.0F, 9.0F, 2.0F, new Dilation(0.0F))
+				.uv(0, 2)
+				.cuboid(-1.0F, -0.5F, 4.5F, 2.0F, 9.0F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -11.5F, 0.5F));
+
+
+		Robot.addChild("Antene", ModelPartBuilder.create().uv(0, 1).cuboid(0.0F, -7.0F, -1.0F, 1.0F, 2.0F, 1.0F, new Dilation(0.0F))
+				.uv(0, 3).cuboid(0.0F, -9.0F, -2.0F, 1.0F, 2.0F, 3.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -8.0F, 1.0F));
+
+		Robot.addChild("Energy", ModelPartBuilder.create().uv(0, 2).cuboid(0.0F, -11.0F, 2.0F, 1.0F, 1.0F, 2.0F, new Dilation(0.0F))
+				.uv(0, 2).cuboid(0.0F, -12.0F, 3.0F, 1.0F, 1.0F, 2.0F, new Dilation(0.0F))
+				.uv(0, 2).cuboid(0.0F, -13.0F, 4.0F, 1.0F, 1.0F, 2.0F, new Dilation(0.0F))
+				.uv(0, 2).cuboid(0.0F, -14.0F, 5.0F, 1.0F, 1.0F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -8.0F, 1.0F));
+
+
 		return TexturedModelData.of(modelData, 64, 64);
 	}
 	@Override
@@ -49,6 +93,9 @@ public class RobotModel<T extends RobotEntity> extends SinglePartEntityModel<T> 
 		Robot.render(matrices, vertexConsumer, light, overlay, red, green, blue, alpha);
 	}
 
+	public ModelPart getRightArm() {
+		return this.rightArm;
+	}
 	@Override
 	public ModelPart getPart() {
 		return this.Robot;
--- src/main/java/siheynde/bachelorproefmod/entity/client/RobotRenderer.java
+++ src/main/java/siheynde/bachelorproefmod/entity/client/RobotRenderer.java
@@ -1,14 +1,18 @@
 package siheynde.bachelorproefmod.entity.client;
 
+import net.minecraft.block.Blocks;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.render.OverlayTexture;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.MobEntityRenderer;
+import net.minecraft.client.render.entity.model.IronGolemEntityModel;
 import net.minecraft.client.render.model.ModelLoader;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.RotationAxis;
 import siheynde.bachelorproefmod.BachelorProef;
 import siheynde.bachelorproefmod.entity.robot.RobotEntity;
 import net.minecraft.client.render.model.json.ModelTransformationMode;
@@ -37,6 +41,7 @@ public class RobotRenderer extends MobEntityRenderer<RobotEntity, RobotModel<Rob
 
         super.render(entity, f, g, matrixStack, vertexConsumerProvider, i);
 
+
         if (entity.getHeldItem() != null) {
             renderItemInHand(matrixStack, vertexConsumerProvider, i, entity);
         }
@@ -45,9 +50,23 @@ public class RobotRenderer extends MobEntityRenderer<RobotEntity, RobotModel<Rob
     private void renderItemInHand(MatrixStack matrixStack, VertexConsumerProvider buffer, int packedLight, RobotEntity entity) {
         // Determine the position and rotation of the hand
         // For example, you might use a combination of entity's position, rotation, and hand position
+        //matrixStack.push();
+        //ModelPart modelPart = ((RobotModel)this.getContextModel()).getRightArm();
+        //ModelPart modelPart = ((IronGolemEntityModel)this.getContextModel()).getRightArm();
+        //modelPart.rotate(matrixStack);
+        matrixStack.translate(-0.9f, 0.05f, -0.5f);
+        matrixStack.translate(0.4f, 0.3f, 0.5f);
+        //float m = 0.5f;
+        matrixStack.scale(0.5f, 0.5f, 0.5f);
+        //matrixStack.multiply(RotationAxis.POSITIVE_X.rotationDegrees(-90.0f));
+        //matrixStack.translate(-0.5f, -0.5f, -0.5f);
+        //this.blockRenderManager.renderBlockAsEntity(Blocks.POPPY.getDefaultState(), matrixStack, buffer, packedLight, OverlayTexture.DEFAULT_UV);
 
         // Use Minecraft's rendering utilities to render the item
         // For example:
-        MinecraftClient.getInstance().getItemRenderer().renderItem(entity.getHeldItem(), ModelTransformationMode.THIRD_PERSON_RIGHT_HAND, packedLight, OverlayTexture.DEFAULT_UV, matrixStack, buffer, null, 0);
+        //ModelTransformationMode.HEAD
+        MinecraftClient.getInstance().getItemRenderer()
+                .renderItem(entity.getHeldItem(),
+                        ModelTransformationMode.FIRST_PERSON_RIGHT_HAND, packedLight, OverlayTexture.DEFAULT_UV, matrixStack, buffer, null, 0);
     }
 }
